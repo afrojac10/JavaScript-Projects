8761c4e36357245452bc6fb815970384fe2372a3
Blaise Njike <chukwudinjike17@gmail.com>
2019-12-29 23:57:01 +0100

    Fix ~  Validating Note Entry

--- To Do App/script.js
+++ To Do App/script.js
@@ -15,9 +15,15 @@ function grabText(){
         TEXTCONTAINER.appendChild(containerOfText);
         GETTEXT.value = null;
 
+    if (noteInputs == "" && noteInputs == " "){
+        GETTEXT.style.outlineColor = "red";
+    }
+
     // console.log(containerOfText);
     }else{
         console.log("Please fill In!");
+        GETTEXT.style.borderColor = "crimson";
+        GETTEXT.placeholder = "Fill In A Value";
     }
     
     
