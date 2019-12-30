19ccf922ef8899b75db482e71c2aba4e9900e6f1
Blaise Njike <chukwudinjike17@gmail.com>
2019-12-30 02:53:44 +0100

    Styling ~  Added Styles

--- To Do App/script.js
+++ To Do App/script.js
@@ -3,7 +3,12 @@
 const   GETTEXT = document.querySelector("textarea");
 const TEXTCONTAINER = document.querySelector(".newNotes");
 const BUTTON = document.querySelector(".addNotes");
+const   HEADER = document.querySelector(".addedText");
+const   HEADERINPUT = document.querySelector("#header");
+
 BUTTON.addEventListener("click" , grabText);
+BUTTON.addEventListener("click" , grabHeader);
+
 
 function grabText(){
     
@@ -15,19 +20,37 @@ function grabText(){
         TEXTCONTAINER.appendChild(containerOfText);
         GETTEXT.value = null;
 
-    if (noteInputs == "" && noteInputs == " "){
-        GETTEXT.style.outlineColor = "red";
-    }
-
-    // console.log(containerOfText);
+    console.log(containerOfText);
     }else{
         console.log("Please fill In!");
         GETTEXT.style.borderColor = "crimson";
         GETTEXT.placeholder = "Fill In A Value";
+        // GETTEXT.classList.add('placeHolderColorChange');
+    }
+
+}
+
+
+
+function grabHeader(){
+    var noteHeader = HEADERINPUT.value;
+    if (noteHeader !== "" && noteHeader !== " " ){
+        var containerOfHeader = document.createElement("h2");
+        var headerText = document.createTextNode(noteHeader);
+        containerOfHeader.appendChild(headerText);
+        HEADER.appendChild(containerOfHeader);
+        HEADERINPUT.value = null;
+
+    
+    console.log(HEADER);
+    }else{
+        console.log("Please fill In!");
+        HEADERINPUT.placeholder = "Fill In A Value";
+        HEADERINPUT.classList.add('placeHolderColorChange');
     }
     
-    
 }
+
 // console.log(BUTTON);
 
 
