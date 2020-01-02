30be932ae2653125a40b4ee38120f9d6c102a97b
Blaise Njike <chukwudinjike17@gmail.com>
2020-01-02 16:59:33 +0100

    Fixed ~  Refactored App Logic

--- To Do App/script.js
+++ To Do App/script.js
@@ -3,7 +3,6 @@
 const   GETTEXT = document.querySelector("textarea");
 const TEXTCONTAINER = document.querySelector(".newNotes");
 const BUTTON = document.querySelector(".addNotes");
-const   HEADER = document.querySelector(".addedText");
 const   HEADERINPUT = document.querySelector("#header");
 
 BUTTON.addEventListener("click" , () =>{
@@ -13,7 +12,7 @@ BUTTON.addEventListener("click" , () =>{
 
 
 function grabHeader(){
-    // Get Header From Field Create New Element And Append To DOM
+    // Get Title From Field Create New Element And Append To DOM
     var noteHeader = HEADERINPUT.value;
         var containerOfHeader = document.createElement("h3");
         var headerText = document.createTextNode(noteHeader);
@@ -53,10 +52,10 @@ function checkEmptyField(){
 function styleRestoreDefault() {
     const noteHeader = HEADERINPUT.value;
     const noteInputs = GETTEXT.value;
-    // Return Note and Header Field To Default Style
+    // Return Note and Title Field To Default Style
     if (noteHeader == "" && " "  && noteInputs == "" &&  " " ){
         GETTEXT.placeholder = "Enter Your Notes Here";
-        //  Return Header Field Default Style
+        //  Return Title Field Default Style
         HEADERINPUT.placeholder = "Enter Note Heading";
         HEADERINPUT.classList.add('inputPlaceHolderSettings');
         
@@ -73,7 +72,7 @@ function styleRestoreDefault() {
 function errMessageIfFieldEmpty(){
     // Error message and warning when field empty
     console.log("Please fill In!");
-    // Header Error Message
+    // Title Error Message
     HEADERINPUT.placeholder = "Fill In A Value";
     HEADERINPUT.classList.add('placeHolderColorChange');
     //  Notes Error Message
