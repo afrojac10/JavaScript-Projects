91b89ebccc21b48aa1ff3100f173faf456838cd8
Blaise Njike <chukwudinjike17@gmail.com>
2020-01-10 11:28:44 +0100

    API's ~  Work stages

--- API's/script.js
+++ API's/script.js
@@ -1,5 +1,5 @@
 console.log("Work Still In Progress");
-// console.log("Planing Stage");
+console.log("Planing Stage");
 console.log("Working on API's");
 console.log("Selecting API's");
 console.log("Intergration");
